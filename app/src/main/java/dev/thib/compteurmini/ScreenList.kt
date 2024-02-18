150d6834b2ecf4e2eca96240e22250f8d6f9f80a
thibault-forret <78164277+thibault-forret@users.noreply.github.com>
2024-02-19 00:01:33 +0100

    add data to object

--- app/src/main/java/dev/thib/compteurmini/ScreenList.kt
+++ app/src/main/java/dev/thib/compteurmini/ScreenList.kt
@@ -1,53 +1,43 @@
 package dev.thib.compteurmini
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Person
-import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
-
 sealed class ScreenList(
     val route: String,
     val title: String,
     val selectedIcon: Int? = null,
     val unSelectedIcon: Int? = null
 ) {
-    object Accueil : ScreenList(
+    data object Accueil : ScreenList(
         route = "accueil",
         title = "Accueil",
         selectedIcon = R.drawable.ic_home_filled,
         unSelectedIcon = R.drawable.ic_home_outlined
     )
 
-    object Inventaire : ScreenList(
+    data object Inventaire : ScreenList(
         route = "inventaire",
         title = "Inventaire",
         selectedIcon = R.drawable.ic_dossier_filled,
         unSelectedIcon = R.drawable.ic_dossier_outlined
     )
 
-    object AjouterPlaque : ScreenList(
+    data object AjouterPlaque : ScreenList(
         route = "ajouter_plaque",
         title = "Ajouter Plaque",
     )
 
-    object DetailPlaque : ScreenList(
+    data object DetailPlaque : ScreenList(
         route = "detail_plaque",
         title = "Détail Plaque",
     )
 
-    object Classement : ScreenList(
+    data object Classement : ScreenList(
         route = "classement",
         title = "Classement",
         selectedIcon = R.drawable.ic_trophee_filled,
-        unSelectedIcon = R.drawable.ic_trophee_outlined//ImageVector.vectorResource(R.drawable.outlined_dossier)
+        unSelectedIcon = R.drawable.ic_trophee_outlined
     )
 
-    object Options : ScreenList(
+    data object Options : ScreenList(
         route = "options",
         title = "Options",
         selectedIcon = R.drawable.ic_options_filled,
